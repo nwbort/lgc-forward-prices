bdfe68e5b297b98f72f51ddb5b8e52713838f359
nwbort <30829548+nwbort@users.noreply.github.com>
2026-01-05 16:27:31 +1100

    Update scraping script to use extract_trades.py
    Redirect output to forward_trades.csv and change script used for extraction.

--- scrape.sh
+++ scrape.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -s 'https://www.demandmanager.com.au/certificate-prices/' | python3 extract_forwards.py > forward_trades.csv
+curl -s 'https://www.demandmanager.com.au/certificate-prices/' | python3 extract_trades.py
